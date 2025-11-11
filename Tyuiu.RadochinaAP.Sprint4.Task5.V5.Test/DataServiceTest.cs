b95051d0434773a9cf6eab671014521808e3f5fb
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-11-11 21:58:18 +0300

    .

---
+++ Tyuiu.RadochinaAP.Sprint4.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.RadochinaAP.Sprint4.Task5.V5.Lib;
+namespace Tyuiu.RadochinaAP.Sprint4.Task5.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] {
+                { 3, -2, 7, 1, -4 },
+                { 5, 0, 8, -1, 2 },
+                { -3, 4, 6, -5, 3 },
+                { 2, 7, -2, 0, 1 },
+                { 8, -4, 5, 2, 6 }
+            };
+            int res = ds.Calculate(mas2);
+            int wait = 70;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RadochinaAP.Sprint4.Task5.V5/Program.cs
@@ -0,0 +1,62 @@
+﻿using Tyuiu.RadochinaAP.Sprint4.Task5.V5.Lib;
+namespace Tyuiu.RadochinaAP.Sprint4.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("**********************************************************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                                                                      *");
+            Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                                                                                            *");
+            Console.WriteLine("* Задание #5                                                                                                                     *");
+            Console.WriteLine("* Вариант #5                                                                                                                     *");
+            Console.WriteLine("* Выполнила: Радочина Арина Павловна | ПИНб-25-1                                                                                 *");
+            Console.WriteLine("**********************************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                                       *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный случайными значениями в диапазоне                                   *");
+            Console.WriteLine("* от -5 до 8. Найти сумму положительных элементов.                                                                                     *");
+            Console.WriteLine("*                                                                                                                                *");
+            Console.WriteLine("**********************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                               *");
+            Console.WriteLine("**********************************************************************************************************************************");
+
+            int rows = 5;
+            int columns = 5;
+
+            int[,] mtrx = new int[rows, columns];
+            Random rnd = new Random();
+
+            // Заполняем случайными числами от -5 до 8
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    mtrx[i, j] = rnd.Next(-5, 9);
+                }
+            }
+
+            Console.WriteLine("Массив 5x5 (случайные числа от -5 до 8):");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("****************************************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                                                *");
+            Console.WriteLine("********************************************************************************************************************************************");
+
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Сумма положительных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
