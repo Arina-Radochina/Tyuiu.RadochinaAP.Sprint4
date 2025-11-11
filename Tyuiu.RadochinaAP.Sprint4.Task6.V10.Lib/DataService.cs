de8c907e68a62ba94123fc6c866e4b6acddd93bd
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-11-11 22:27:52 +0300

    .

---
+++ Tyuiu.RadochinaAP.Sprint4.Task6.V10.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.RadochinaAP.Sprint4.Task6.V10.Lib
+{
+    public class DataService : ISprint4Task6V10
+    {
+        public string[] Calculate(string[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length < 7)
+                {
+                    count++;
+                }
+            }
+
+            string[] result = new string[count];
+            int index = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length < 7)
+                {
+                    result[index] = array[i];
+                    index++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.RadochinaAP.Sprint4.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.RadochinaAP.Sprint4.Task6.V10.Lib;
+namespace Tyuiu.RadochinaAP.Sprint4.Task6.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            string[] array = { "Компьютер", "Телефон", "Мышь", "Клавиатура", "Монитор", "Принтер" };
+            string[] res = ds.Calculate(array);
+
+            // Элементы длиной < 7: только "Мышь"
+            string[] wait = { "Мышь" };
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
