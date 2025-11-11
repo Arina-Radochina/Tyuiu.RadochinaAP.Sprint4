427ca2c5bb7bf7194c5ededb0e21d2cf92bca152
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-11-11 21:14:27 +0300

    .

---
+++ Tyuiu.RadochinaAP.Sprint4.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.RadochinaAP.Sprint4.Task3.V12.Lib
+{
+    public class DataService : ISprint4Task3V12
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1; 
+            int columns = matrix.Length / rows; 
+
+            int min = int.MaxValue;
+            int columnIndex = 2; 
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (matrix[i, columnIndex] < min)
+                {
+                    min = matrix[i, columnIndex];
+                }
+            }
+            return min;
+        }
+    }
+}
