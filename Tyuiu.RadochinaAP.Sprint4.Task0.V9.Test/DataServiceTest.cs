c68ea9e6beb5f7dd3430860ccdab5585cc05cf8a
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-11-11 20:10:10 +0300

    Add project files.

---
+++ Tyuiu.RadochinaAP.Sprint4.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RadochinaAP.Sprint4.Task0.V9.Lib;
+namespace Tyuiu.RadochinaAP.Sprint4.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumEvenArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 4, 6, 2, 8, 4, 5, 6, 9, 8, 7 };
+            int res = ds.GetSumEvenArrEl(numsArray);
+            int wait = 38;
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
