1f62112cc6666d10495d13e98edc1426052f757b
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-11-11 20:51:44 +0300

    .

---
+++ Tyuiu.RadochinaAP.Sprint4.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.RadochinaAP.Sprint4.Task2.V4.Lib;
+namespace Tyuiu.RadochinaAP.Sprint4.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 3, 4, 5, 7, 2, 9, 6, 3, 8, 5, 7, 4, 9 };
+
+            int res = ds.Calculate(numsArray);
+            int wait = 48;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.RadochinaAP.Sprint4.Task2.V4/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.RadochinaAP.Sprint4.Task2.V4.Lib;
+namespace Tyuiu.RadochinaAP.Sprint4.Task2.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+            Console.WriteLine("**********************************************************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                                                                      *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                                                                                    *");
+            Console.WriteLine("* Задание #2                                                                                                                     *");
+            Console.WriteLine("* Вариант #4                                                                                                                      *");
+            Console.WriteLine("* Выполнила: Радочина Арина Павловна | ПИНб-25-1                                                                                 *");
+            Console.WriteLine("**********************************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                                       *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный случайными в диапазоне                                         *");
+            Console.WriteLine("* от 2 до 9 подсчитать сумму нечетных элементов массива.                                                                         *");
+            Console.WriteLine("*                                                                                                                                *");
+            Console.WriteLine("**********************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                               *");
+            Console.WriteLine("**********************************************************************************************************************************");
+
+            Console.Write("Введите количество элементов массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                numsArray[i] = rnd.Next(5, 20);
+            }
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(numsArray[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine("*******************************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                                  *");
+            Console.WriteLine("*******************************************************************************************************************************");
+
+            int res = ds.Calculate(numsArray);
+
+            Console.WriteLine("Сумма элементов больше 10 = " + res);
+            Console.ReadKey();
+        }
+    }
+}
